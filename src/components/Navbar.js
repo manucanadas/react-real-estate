6a8e2774126718ec2699b2ce5ff8db05ceb8d227
Manuel Canadas <nanocanadas@gmail.com>
2021-05-07 15:44:57 +1200

    Navbar styled and menu button created

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,76 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components/macro';
 import { Link } from 'react-router-dom';
 import { menuData } from '../data/MenuData';
+import {Button} from './Button';
+import { FaBars } from 'react-icons/fa';
 
 const Nav = styled.nav`
   height: 60px;
   display: flex;
-  background: #000;
+  justify-content: space-between;
+  padding: 1rem 2rem;
+  z-index: 100;
+  position: fixed;
+  width: 100%;
+  background: red;
+`;
+
+const NavLink = css`
+  color: #fff;
+  display: flex;
+  align-items: center;
+  padding: 0 1rem;
+  height: 100%;
+  cursor: pointer;
+  text-decoration: none;
 `;
 
 const Logo = styled(Link)`
-  color: #fff;
+  ${NavLink}
+  font-style: italic;
 `;
 
-const MenuBars = styled.i``;
+const MenuBars = styled(FaBars)`
+  display: none;
 
-const NavMenu = styled.div``;
+  @media screen and (max-width: 768px) {
+    display: block;
+    color: #fff;
+    background-size: contain;
+    height: 30px;
+    width: 30px;
+    cursor: pointer;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-50%, 50%);
+  }
+`;
+
+const NavMenu = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: -48px;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
 
 const NavMenuLinks = styled(Link)`
-color: #fff;
-`
+  ${NavLink}
+`;
+
+const NavBtn = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: 24px;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
 
 const Navbar = () => {
     return (
@@ -33,6 +84,9 @@ const Navbar = () => {
                     </NavMenuLinks>
                 ))}
             </NavMenu>
+            <NavBtn>
+                <Button to='/contact' primary='true'>Contact Us</Button>
+            </NavBtn>
         </Nav>
     );
 };
