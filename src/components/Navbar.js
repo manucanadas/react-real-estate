8d3e633f17e121d330165fb2b116f206c92c3fe0
Manuel Canadas <nanocanadas@gmail.com>
2021-05-07 14:15:36 +1200

    Navbar links

---
+++ src/components/Navbar.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import { menuData } from '../data/MenuData';
+
+const Nav = styled.nav`
+  height: 60px;
+  display: flex;
+  background: #000;
+`;
+
+const Logo = styled(Link)`
+  color: #fff;
+`;
+
+const MenuBars = styled.i``;
+
+const NavMenu = styled.div``;
+
+const NavMenuLinks = styled(Link)`
+color: #fff;
+`
+
+const Navbar = () => {
+    return (
+        <Nav>
+            <Logo to='/'>MANU</Logo>
+            <MenuBars />
+            <NavMenu>
+                {menuData.map((item, index) => (
+                    <NavMenuLinks to={item.link} key={index}>
+                        {item.title}
+                    </NavMenuLinks>
+                ))}
+            </NavMenu>
+        </Nav>
+    );
+};
+
+export default Navbar
+
