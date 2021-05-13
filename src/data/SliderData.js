68c14e4f563096e0af065206943f7e8077d1eade
Manuel Canadas <nanocanadas@gmail.com>
2021-05-13 21:01:56 +1200

    Hero section and some SliderData

--- src/data/SliderData.js
+++ src/data/SliderData.js
@@ -1,3 +1,8 @@
+import ImageOne from '../images/house-1.jpg';
+import ImageTwo from '../images/house-2.jpg';
+import ImageThree from '../images/house-3.jpg';
+import ImageFour from '../images/house-4.jpg';
+
 export  const SliderData = [
     {
       title: 'Luxury Villa in Bali, Indonesia',
@@ -6,5 +11,29 @@ export  const SliderData = [
       label: 'View Home',
       image: ImageOne,
       alt: 'House'
-    }
+    },
+    {
+        title: 'House in San San Jose, Costa Rica',
+        price: '$2,860,000',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageTwo,
+        alt: 'House'
+      },
+      {
+        title: 'House in Calabasas, California',
+        price: '$8,420,000',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageThree,
+        alt: 'House'
+      },
+      {
+        title: 'House in Houston, Texas',
+        price: '$6,660,000',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageFour,
+        alt: 'House'
+      },
 ]
