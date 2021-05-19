df8ca7420d8f48c692a196cf4a6cfb995eb4b18b
Manuel Canadas <nanocanadas@gmail.com>
2021-05-19 16:12:01 +1200

    Dropdown component

---
+++ src/components/Dropdown.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import styled from 'styled-components';
+import { menuData } from '../data/MenuData';
+import { Button } from './Button';
+import { Link } from 'react-router-dom';
+import { FaTimes } from 'react-icons/fa';
+
+const DropdownContainer = styled.div`
+  position: fixed;
+  z-index: 999;
+  width: 100%;
+  height: 100%;
+  background: #cd853f;
+  display: grid;
+  align-items: center;
+  top: 0;
+  left: 0;
+  transition: 0.3s ease-in-out;
+  opacity: 1;
+  top: 0;
+`;
+
+const Icon = styled.div``;
+const CloseIcon = styled(FaTimes)``;
+const DropdownWrapper = styled.div``;
+const DropdownMenu = styled.div``;
+const DropdownLink = styled(Link)``;
+const BtnWrap = styled.div``;
+
+const Dropdown = () => {
+    return (
+        <DropdownContainer>
+            <Icon>
+              <CloseIcon />  
+            </Icon>
+            <DropdownWrapper>
+              <DropdownMenu>
+                  {menuData.map((item, index) => {
+                    <DropdownLink to={item.link} key={index}>
+                      {item.title}
+                    </DropdownLink> 
+                  })}
+              </DropdownMenu>
+              <BtnWrap>
+                <Button primary='true' round='true' big='true' to='/contact'>
+                  Contac Us
+                </Button>
+              </BtnWrap>  
+            </DropdownWrapper>
+        </DropdownContainer>
+    )
+}
+
+export default Dropdown
