e422d7ff9ad072153a0a6e21150dbb8e7a96bb58
Manuel Canadas <nanocanadas@gmail.com>
2021-05-20 17:00:17 +1200

    InfoSection

---
+++ src/data/InfoData.js
@@ -0,0 +1,22 @@
+import ImageOne from '../images/interior-1.jpg';
+import ImageTwo from '../images/house-5.jpg';
+
+export const InfoData = {
+  heading: 'Explore our beautiful homes',
+  paragraphOne: 'lorem la kjaofhoehf ouefh ouhcuoe foun ewhf oihfoiewh fou hefi oiewhfwli gvsf d  jhfoh ioeufh uh iy uydgf iwuegiuegh h oie uhoueh oiuh ',
+  paragraphTwo: 'lorem la kjaofhoehf ouefh ouhcuoe foun ewhf oihfoiewh fou hefi oiewhfwlih oie uhoueh oiuh',
+  buttonLabel: 'View Homes',
+  image: ImageOne,
+  reverse: false,
+  delay: 100,
+};
+
+export const InfoDataTwo = {
+  heading: 'Modern Designs',
+  paragraphOne: 'lorem la kjaofhoehf ouefh ouhcuofou hefi oiewhfwli gvsf d  jhfewludfukh ua fiy gieyg iuehf eufh uh iy uydgf iwuegiuegh h oie uhoueh oiuh ',
+  paragraphTwo: 'lorem la kjaoiydfi oun ewhf oihfoiewh fou hefi oiew uiahefuo efhoah hfwlih oie uhoueh oiuh',
+  buttonLabel: 'View Homes',
+  image: ImageTwo,
+  reverse: true,
+  delay: 300,
+};
